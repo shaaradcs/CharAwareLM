de193cbeacc80db7fb726452976c6782515deb17
shaaradcs <rangashaarad@gmail.com>
2019-04-29 18:25:00 +0530

    Slight cleaning

--- train.py
+++ train.py
@@ -6,16 +6,16 @@ import time
 from tqdm import tqdm
 import module
 
-"""
 if torch.cuda.is_available():
     print('GPU available')
     print('Current(default) GPU device : ' + str(torch.cuda.current_device()))
+    """
     device = 1
     torch.cuda.set_device(device)
     print('GPU device set to ' + str(torch.cuda.current_device()))
+    """
 else:
     print('GPU unavailable. Using CPU')
-"""
 
 # Initialize model
 model = module.LanguageModel()
@@ -25,12 +25,10 @@ if torch.cuda.is_available():
     model.cuda()
     model = nn.DataParallel(model)
 
-
 # Loading training data in the form of list of tensor tuples
 data = pickle.load(open('data/train.txt.pkl', 'rb'))
 print('Training data loaded')
 
-# valid_data = pickle.load(open('data/valid.txt.pkl', 'rb'))
 valid_data = pickle.load(open('data/valid.txt.pkl', 'rb'))
 print('Validation data loaded')
 
@@ -51,6 +49,7 @@ for epoch in range(0, 30):
     # Pass through the entire training data once, in batches
     # Also, calculate the average loss over all sentences
     print('Training')
+    model.train()
     training_loss = torch.zeros(1).float().cuda()
     for step in tqdm(range(0, len(data)//batch_size)):
         offset = (step * batch_size) % (len(data) - batch_size)
@@ -67,30 +66,34 @@ for epoch in range(0, 30):
         results = model(char_index)
         # results_1, results_2 = model(char_index)
         for i in range(0, batch_size):
-            train_loss += criterion(results[i], word_index[i][1:word_index[1].shape[0]-1])
-            """
-            train_loss += criterion(results_1[i][1:word_index[i].shape[0]-1], word_index[i][2:word_index[i].shape[0]])
-            train_loss += criterion(results_2[i][1:word_index[i].shape[0]-1], word_index[i][0:word_index[i].shape[0]-2])
-            """
+            train_loss += criterion(results[i], word_index[i][1:-1])
         train_loss.backward()
         torch.nn.utils.clip_grad_norm_(model.parameters(), 5.0)
         optim.step()
         training_loss += train_loss
     training_loss = training_loss / (len(data) // batch_size)
     
+
+
     print('Validation')
-    validation_loss = torch.zeros(1).float().cuda()
     # Calculate the perplexity on the validation set
+    model.eval()
     with torch.no_grad():
         if torch.cuda.is_available():
             perplexity = torch.zeros(1).float().cuda()
+            validation_loss = torch.zeros(1).float().cuda()
         else:
             perplexity = torch.zeros(1).float()
+            validation_loss = torch.zeros(1).float()
         for line in tqdm(valid_data):
-            char_index = line[0].view(1, line[0].shape[0], -1).cuda()
-            word_index = line[1].cuda()
+            if torch.cuda.is_available():
+                char_index = line[0].view(1, line[0].shape[0], -1).cuda()
+                word_index = line[1].cuda()
+            else:
+                char_index = line[0].view(1, line[0].shape[0], -1)
+                word_index = line[1]
             result = model(char_index)
-            loss = criterion(result[0], word_index[1:word_index.shape[0]-1])
+            loss = criterion(result[0], word_index[1:-1])
             """
             result_1, result_2 = model(char_index)
             loss = criterion(result_1[0][1:word_index.shape[0]-1], word_index[2:word_index.shape[0]])
@@ -109,4 +112,4 @@ for epoch in range(0, 30):
 
     # Save current model to a file
     model.__module__ = 'module'
-    pickle.dump(model, open('model_82/epoch_' + str(epoch + 1) + '.pkl','wb'))
+    pickle.dump(model, open('models/model_82/epoch_' + str(epoch + 1) + '.pkl','wb'))
